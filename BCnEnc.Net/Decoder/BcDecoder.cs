4fa6fca0d414363c48f4b8329e246b8131f9e811
Nominom <nomi.lakkala@gmail.com>
2020-04-21 17:31:48 +0300

    Reading DDS files working for most common formats.

--- BCnEnc.Net/Decoder/BcDecoder.cs
+++ BCnEnc.Net/Decoder/BcDecoder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 using BCnEnc.Net.Shared;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Advanced;
@@ -37,6 +38,19 @@ namespace BCnEnc.Net.Decoder
 			}
 		}
 
+		private bool IsSupportedRawFormat(DXGI_FORMAT format)
+		{
+			switch (format)
+			{
+				case DXGI_FORMAT.DXGI_FORMAT_R8_UNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_R8G8_UNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_R8G8B8A8_UNORM:
+					return true;
+				default:
+					return false;
+			}
+		}
+
 		private IBcBlockDecoder GetDecoder(GlInternalFormat format)
 		{
 			switch (format)
@@ -63,6 +77,39 @@ namespace BCnEnc.Net.Decoder
 			}
 		}
 
+		private IBcBlockDecoder GetDecoder(DXGI_FORMAT format)
+		{
+			switch (format)
+			{
+				case DXGI_FORMAT.DXGI_FORMAT_BC1_UNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_BC1_UNORM_SRGB:
+				case DXGI_FORMAT.DXGI_FORMAT_BC1_TYPELESS:
+					return new Bc1ADecoder();
+				case DXGI_FORMAT.DXGI_FORMAT_BC2_UNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_BC2_UNORM_SRGB:
+				case DXGI_FORMAT.DXGI_FORMAT_BC2_TYPELESS:
+					return new Bc2Decoder();
+				case DXGI_FORMAT.DXGI_FORMAT_BC3_UNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_BC3_UNORM_SRGB:
+				case DXGI_FORMAT.DXGI_FORMAT_BC3_TYPELESS:
+					return new Bc3Decoder();
+				case DXGI_FORMAT.DXGI_FORMAT_BC4_UNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_BC4_SNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_BC4_TYPELESS:
+					return new Bc4Decoder(OutputOptions.redAsLuminance);
+				case DXGI_FORMAT.DXGI_FORMAT_BC5_UNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_BC5_SNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_BC5_TYPELESS:
+					return new Bc5Decoder();
+				case DXGI_FORMAT.DXGI_FORMAT_BC7_UNORM:
+				case DXGI_FORMAT.DXGI_FORMAT_BC7_UNORM_SRGB:
+				case DXGI_FORMAT.DXGI_FORMAT_BC7_TYPELESS:
+					return new Bc7Decoder();
+				default:
+					return null;
+			}
+		}
+
 		private IRawDecoder GetRawDecoder(GlInternalFormat format)
 		{
 			switch (format)
@@ -80,12 +127,107 @@ namespace BCnEnc.Net.Decoder
 			}
 		}
 
+		private IRawDecoder GetRawDecoder(DXGI_FORMAT format)
+		{
+			switch (format)
+			{
+				case DXGI_FORMAT.DXGI_FORMAT_R8_UNORM:
+					return new RawRDecoder(OutputOptions.redAsLuminance);
+				case DXGI_FORMAT.DXGI_FORMAT_R8G8_UNORM:
+					return new RawRGDecoder();
+				case DXGI_FORMAT.DXGI_FORMAT_R8G8B8A8_UNORM:
+					return new RawRGBADecoder();
+				default:
+					return null;
+			}
+		}
+
 		/// <summary>
-		/// Read a KtxFile from a stream and decode it.
+		/// Read a Ktx or a Dds file from a stream and decode it.
 		/// </summary>
 		public Image<Rgba32> Decode(Stream inputStream) {
-			KtxFile file = KtxFile.Load(inputStream);
-			return Decode(file);
+			var position = inputStream.Position;
+			try {
+				if (inputStream is FileStream fs) {
+					var extension = Path.GetExtension(fs.Name).ToLower();
+					if (extension == ".ktx") {
+						KtxFile file = KtxFile.Load(inputStream);
+						return Decode(file);
+					}
+					else if (extension == ".dds") {
+						DdsFile file = DdsFile.Load(inputStream);
+						return Decode(file);
+					}
+				}
+
+				bool isDDS = false;
+				using (var br = new BinaryReader(inputStream, Encoding.UTF8, true)) {
+					var magic = br.ReadUInt32();
+					if (magic == 0x20534444U) {
+						isDDS = true;
+					}
+				}
+
+				inputStream.Seek(position, SeekOrigin.Begin);
+
+				if (isDDS) {
+					DdsFile dds = DdsFile.Load(inputStream);
+					return Decode(dds);
+				}
+				else {
+					KtxFile ktx = KtxFile.Load(inputStream);
+					return Decode(ktx);
+				}
+			}
+			catch (Exception ex) {
+				inputStream.Seek(position, SeekOrigin.Begin);
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Read a Ktx or a Dds file from a stream and decode it.
+		/// </summary>
+		public Image<Rgba32>[] DecodeAllMipMaps(Stream inputStream) {
+			var position = inputStream.Position;
+			try {
+				if (inputStream is FileStream fs) {
+					var extension = Path.GetExtension(fs.Name).ToLower();
+					if (extension == ".ktx") {
+						KtxFile file = KtxFile.Load(inputStream);
+						return DecodeAllMipMaps(file);
+					}
+					else if (extension == ".dds") {
+						DdsFile file = DdsFile.Load(inputStream);
+						return DecodeAllMipMaps(file);
+					}
+				}
+
+				bool isDDS = false;
+				using (var br = new BinaryReader(inputStream, Encoding.UTF8, true)) {
+					var magic = br.ReadUInt32();
+					if (magic == 0x20534444U) {
+						isDDS = true;
+					}
+				}
+
+				inputStream.Seek(position, SeekOrigin.Begin);
+
+				if (isDDS) {
+					DdsFile dds = DdsFile.Load(inputStream);
+					return DecodeAllMipMaps(dds);
+				}
+				else {
+					KtxFile ktx = KtxFile.Load(inputStream);
+					return DecodeAllMipMaps(ktx);
+				}
+			}
+			catch (Exception ex) {
+				inputStream.Seek(position, SeekOrigin.Begin);
+			}
+
+			return null;
 		}
 
 		/// <summary>
@@ -119,7 +261,159 @@ namespace BCnEnc.Net.Decoder
 
 				var blocks = decoder.Decode(data, (int)pixelWidth, (int)pixelHeight, out var blockWidth, out var blockHeight);
 
-				return ImageToBlocks.ImageFromRawBlocks(blocks, blockWidth, blockHeight);
+				return ImageToBlocks.ImageFromRawBlocks(blocks, blockWidth, blockHeight, 
+					(int)pixelWidth, (int)pixelHeight);
+			}
+		}
+
+		/// <summary>
+		/// Read a KtxFile and decode it.
+		/// </summary>
+		public Image<Rgba32>[] DecodeAllMipMaps(KtxFile file)
+		{
+			if (IsSupportedRawFormat(file.Header.GlInternalFormat)) {
+				var decoder = GetRawDecoder(file.Header.GlInternalFormat);
+				var images = new Image<Rgba32>[file.MipMaps.Count];
+
+				for (int mip = 0; mip < file.MipMaps.Count; mip++) {
+					var data = file.MipMaps[mip].Faces[0].Data;
+					var pixelWidth = file.MipMaps[mip].Width;
+					var pixelHeight = file.MipMaps[mip].Height;
+
+					var image = new Image<Rgba32>((int)pixelWidth, (int)pixelHeight);
+					var output = decoder.Decode(data, (int)pixelWidth, (int)pixelHeight);
+					var pixels = image.GetPixelSpan();
+
+					output.CopyTo(pixels);
+					images[mip] = image;
+				}
+				
+				return images;
+			}
+			else {
+				var decoder = GetDecoder(file.Header.GlInternalFormat);
+				if (decoder == null)
+				{
+					throw new NotSupportedException($"This format is not supported: {file.Header.GlInternalFormat}");
+				}
+				var images = new Image<Rgba32>[file.MipMaps.Count];
+
+				for (int mip = 0; mip < file.MipMaps.Count; mip++) {
+
+					var data = file.MipMaps[mip].Faces[0].Data;
+					var pixelWidth = file.MipMaps[mip].Width;
+					var pixelHeight = file.MipMaps[mip].Height;
+
+					var blocks = decoder.Decode(data, (int) pixelWidth, (int) pixelHeight, out var blockWidth,
+						out var blockHeight);
+
+					var image = ImageToBlocks.ImageFromRawBlocks(blocks, blockWidth, blockHeight, 
+						(int)pixelWidth, (int)pixelHeight);
+					images[mip] = image;
+				}
+				return images;
+			}
+		}
+
+		/// <summary>
+		/// Read a DdsFile and decode it
+		/// </summary>
+		public Image<Rgba32> Decode(DdsFile file)
+		{
+			if (IsSupportedRawFormat(file.Header.ddsPixelFormat.DxgiFormat)) {
+				var decoder = GetRawDecoder(file.Header.ddsPixelFormat.DxgiFormat);
+				var data = file.Faces[0].MipMaps[0].Data;
+				var pixelWidth = file.Faces[0].Width;
+				var pixelHeight = file.Faces[0].Height;
+
+				var image = new Image<Rgba32>((int)pixelWidth, (int)pixelHeight);
+				var output = decoder.Decode(data, (int)pixelWidth, (int)pixelHeight);
+				var pixels = image.GetPixelSpan();
+
+				output.CopyTo(pixels);
+				return image;
+			}
+			else {
+				DXGI_FORMAT format = DXGI_FORMAT.DXGI_FORMAT_UNKNOWN;
+				if (file.Header.ddsPixelFormat.IsDxt10Format) {
+					format = file.Dxt10Header.dxgiFormat;
+				}
+				else {
+					format = file.Header.ddsPixelFormat.DxgiFormat;
+				}
+				IBcBlockDecoder decoder = GetDecoder(format);
+				
+				if (decoder == null)
+				{
+					throw new NotSupportedException($"This format is not supported: {format}");
+				}
+
+				var data = file.Faces[0].MipMaps[0].Data;
+				var pixelWidth = file.Faces[0].Width;
+				var pixelHeight = file.Faces[0].Height;
+
+				var blocks = decoder.Decode(data, (int)pixelWidth, (int)pixelHeight, out var blockWidth, out var blockHeight);
+
+				return ImageToBlocks.ImageFromRawBlocks(blocks, blockWidth, blockHeight, 
+					(int)pixelWidth, (int)pixelHeight);
+			}
+		}
+
+		/// <summary>
+		/// Read a DdsFile and decode it
+		/// </summary>
+		public Image<Rgba32>[] DecodeAllMipMaps(DdsFile file)
+		{
+			if (IsSupportedRawFormat(file.Header.ddsPixelFormat.DxgiFormat)) {
+				var decoder = GetRawDecoder(file.Header.ddsPixelFormat.DxgiFormat);
+
+				var images = new Image<Rgba32>[file.Header.dwMipMapCount];
+
+				for (int mip = 0; mip < file.Header.dwMipMapCount; mip++) {
+					var data = file.Faces[0].MipMaps[mip].Data;
+					var pixelWidth = file.Faces[0].MipMaps[mip].Width;
+					var pixelHeight = file.Faces[0].MipMaps[mip].Height;
+
+					var image = new Image<Rgba32>((int) pixelWidth, (int) pixelHeight);
+					var output = decoder.Decode(data, (int) pixelWidth, (int) pixelHeight);
+					var pixels = image.GetPixelSpan();
+
+					output.CopyTo(pixels);
+					images[mip] = image;
+				}
+				return images;
+			}
+			else {
+				DXGI_FORMAT format = DXGI_FORMAT.DXGI_FORMAT_UNKNOWN;
+				if (file.Header.ddsPixelFormat.IsDxt10Format) {
+					format = file.Dxt10Header.dxgiFormat;
+				}
+				else {
+					format = file.Header.ddsPixelFormat.DxgiFormat;
+				}
+				IBcBlockDecoder decoder = GetDecoder(format);
+				
+				if (decoder == null)
+				{
+					throw new NotSupportedException($"This format is not supported: {format}");
+				}
+				var images = new Image<Rgba32>[file.Header.dwMipMapCount];
+
+				for (int mip = 0; mip < file.Header.dwMipMapCount; mip++) {
+					var data = file.Faces[0].MipMaps[mip].Data;
+					var pixelWidth = file.Faces[0].MipMaps[mip].Width;
+					var pixelHeight = file.Faces[0].MipMaps[mip].Height;
+
+					var blocks = decoder.Decode(data, (int) pixelWidth, (int) pixelHeight, out var blockWidth,
+						out var blockHeight);
+
+					var image = ImageToBlocks.ImageFromRawBlocks(blocks, blockWidth, blockHeight, 
+						(int)pixelWidth, (int)pixelHeight);
+
+					images[mip] = image;
+				}
+
+				return images;
 			}
 		}
 	}
---
+++ BCnEncTests/DdsReadTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BCnEnc.Net.Decoder;
+using BCnEnc.Net.Shared;
+using SixLabors.ImageSharp;
+using Xunit;
+
+namespace BCnEncTests
+{
+	public class DdsReadTests
+	{
+		[Fact]
+		public void ReadRgba() {
+			using FileStream fs = File.OpenRead(@"../../../testImages/test_decompress_rgba.dds");
+			DdsFile file = DdsFile.Load(fs);
+			Assert.Equal(DXGI_FORMAT.DXGI_FORMAT_R8G8B8A8_UNORM, file.Header.ddsPixelFormat.DxgiFormat);
+			Assert.Equal(file.Header.dwMipMapCount, (uint)file.Faces[0].MipMaps.Length);
+
+			BcDecoder decoder = new BcDecoder();
+			var images = decoder.DecodeAllMipMaps(file);
+
+			Assert.Equal((uint)images[0].Width, file.Header.dwWidth);
+			Assert.Equal((uint)images[0].Height, file.Header.dwHeight);
+
+			for (int i = 0; i < images.Length; i++) {
+				using FileStream outFs = File.OpenWrite($"decoding_test_dds_rgba_mip{i}.png");
+				images[i].SaveAsPng(outFs);
+				images[i].Dispose();
+			}
+		}
+
+		[Fact]
+		public void ReadBc1() {
+			using FileStream fs = File.OpenRead(@"../../../testImages/test_decompress_bc1.dds");
+			DdsFile file = DdsFile.Load(fs);
+			Assert.Equal(DXGI_FORMAT.DXGI_FORMAT_BC1_UNORM, file.Header.ddsPixelFormat.DxgiFormat);
+			Assert.Equal(file.Header.dwMipMapCount, (uint)file.Faces[0].MipMaps.Length);
+
+
+			BcDecoder decoder = new BcDecoder();
+			var images = decoder.DecodeAllMipMaps(file);
+
+			Assert.Equal((uint)images[0].Width, file.Header.dwWidth);
+			Assert.Equal((uint)images[0].Height, file.Header.dwHeight);
+
+			for (int i = 0; i < images.Length; i++) {
+				using FileStream outFs = File.OpenWrite($"decoding_test_dds_bc1_mip{i}.png");
+				images[i].SaveAsPng(outFs);
+				images[i].Dispose();
+			}
+		}
+
+		[Fact]
+		public void ReadBc7() {
+			using FileStream fs = File.OpenRead(@"../../../testImages/test_decompress_bc7.dds");
+			BcDecoder decoder = new BcDecoder();
+			var images = decoder.DecodeAllMipMaps(fs);
+
+			for (int i = 0; i < images.Length; i++) {
+				using FileStream outFs = File.OpenWrite($"decoding_test_dds_bc7_mip{i}.png");
+				images[i].SaveAsPng(outFs);
+				images[i].Dispose();
+			}
+		}
+	}
+}
