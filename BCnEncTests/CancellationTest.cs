5636ca1541790979cccb049b1e2fa6d79002c1d0
onepiecefreak3 <alberteinstein1980@gmx.de>
2021-01-15 23:45:53 +0100

    Remove decoding cancellation tests;

--- BCnEncTests/CancellationTest.cs
+++ BCnEncTests/CancellationTest.cs
@@ -20,20 +20,8 @@ namespace BCnEncTests
 			await TestHelper.ExecuteCancellationTest(ImageLoader.TestAlphaGradient1, false);
 		}
 
-		// HINT: Parallel decoding is too fast to be cancelled.
-		// HINT: Even with TimeSpan.FromTicks(1) the test never successfully threw an exception when executed in bulk with other tests.
-
-		[Fact]
-		public async void DecodeNonParallelCancellation()
-		{
-			var file = DdsLoader.TestDecompressBc7;
-
-			var decoder = new BcDecoder();
-			decoder.Options.IsParallel = false;
-
-			var source = new CancellationTokenSource(TimeSpan.FromMilliseconds(1));
-			await Assert.ThrowsAnyAsync<OperationCanceledException>(() =>
-					decoder.DecodeAsync(file, source.Token));
-		}
+		// HINT: Decoding in general is too fast to be cancelled.
+		// HINT: For parallel decoding even with TimeSpan.FromTicks(1) the test never successfully threw an exception when executed in bulk with other tests.
+		// HINT: For non parallel decoding the test was partially successful, due to fluctuations in how much time the decoding needed and when the cancellation was introduced.
 	}
 }
