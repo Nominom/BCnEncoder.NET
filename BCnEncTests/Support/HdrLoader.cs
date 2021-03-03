871747dcff07ff074dfc074ffac9b936139cdbd8
Nominom <nomi.lakkala@gmail.com>
2021-03-03 10:58:25 +0200

    Test filenames changed to lowercase

--- BCnEncTests/Support/HdrLoader.cs
+++ BCnEncTests/Support/HdrLoader.cs
@@ -12,9 +12,9 @@ namespace BCnEncTests.Support
 		public static HdrImage TestHdrKiara { get; } = HdrImage.Read("../../../testImages/test_hdr_kiara.hdr");
 		public static Image<Rgba32> ReferenceKiara { get; } = ImageLoader.LoadTestImage("../../../testImages/test_hdr_kiara.png");
 		public static DdsFile TestHdrKiaraDds { get; } =
-			DdsLoader.LoadDdsFile("../../../testImages/test_hdr_kiara_bc6h.DDS");
+			DdsLoader.LoadDdsFile("../../../testImages/test_hdr_kiara_bc6h.dds");
 		public static KtxFile TestHdrKiaraKtx { get; } =
-			KtxLoader.LoadKtxFile("../../../testImages/test_hdr_kiara_bc6h.KTX");
+			KtxLoader.LoadKtxFile("../../../testImages/test_hdr_kiara_bc6h_ktx.ktx");
 		
 	}
 }
